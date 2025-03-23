346a74a349a690e86641f6f982ffa74692f5ef0c
taqisherazi <78368322+taqisherazi@users.noreply.github.com>
2025-03-23 11:12:12 +0500

    Add files via upload

---
+++ analysis_dashboard/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.shortcuts import render, redirect
+
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import login
+
+from django.contrib.auth.decorators import login_required
+
+def analysis_dashboard(request):
+    return render(request, 'analysis_dashboard.html')
