6553abdc7c70245eaa52aee75ec3818cd0249a23
Romana Marvi Rashid <81105743+Romanamarvi@users.noreply.github.com>
2025-04-02 09:11:00 +0500

    Add files via upload

--- TogatherCultureUK/settings.py
+++ TogatherCultureUK/settings.py
@@ -72,14 +72,18 @@ WSGI_APPLICATION = 'TogatherCultureUK.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'togethercultureuk',
+        'USER': 'aws',
+        'PASSWORD': 'Abcd6471@786',
+        'HOST': 'localhost',  # or your MySQL server IP
+        'PORT': '3306',       # default MySQL port
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        }
     }
 }
-
-
-# Password validation
-# https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
